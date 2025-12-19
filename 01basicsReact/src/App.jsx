f3d06ad4fc3aded3aace3551f77f8ba3ae7776e9
mtprxxd <109460576+mtprxxd@users.noreply.github.com>
2025-12-19 12:34:40 +0000

    react flow configrated

--- 01basicsReact/src/App.jsx
+++ 01basicsReact/src/App.jsx
@@ -1,11 +1,26 @@
 
 
-function App() {
+// function App() {
 
-  return (
-    <h1>React-App</h1>
+//   return (
+//     <h1>React-App</h1>
 
     
+//   )
+// }
+
+// export default App
+
+
+import Pune from "./Pune"
+
+function App(){ 
+
+  return (
+    <>
+    <Pune />
+  <h2>Mani London gya</h2>
+  </>
   )
 }
 
---
+++ 01basicsReact/src/Pune.jsx
@@ -0,0 +1,8 @@
+function Pune(){
+
+  return (
+  <h2>Mani Pune gya</h2>
+  )
+}
+
+export default Pune
