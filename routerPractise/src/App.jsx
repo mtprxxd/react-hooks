f8951d9a1567c6f43fe86ea5aed1a7548c512f31
mtprxxd <109460576+mtprxxd@users.noreply.github.com>
2025-12-30 19:04:58 +0000

    practiced accomplished

--- routerPractise/src/App.jsx
+++ routerPractise/src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+import { Outlet } from 'react-router-dom'
 import './App.css'
+import Header from './Components/Header/Header'
+import Footer from './Components/Footer/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
+   
    <>
-   </>
+    <Header />
+    <Outlet />
+    <Footer />
+    </>
+   
   )
 }
 
