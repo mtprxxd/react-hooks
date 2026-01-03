09e2a6c09eee6017b62bc63f9577c9cf00d6c180
mtprxxd <109460576+mtprxxd@users.noreply.github.com>
2026-01-03 09:03:09 +0000

    Card-theme accomplished

--- ThemeToggler/src/App.jsx
+++ ThemeToggler/src/App.jsx
@@ -1,28 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import ThemeButton from './components/ThemeButton'
+import Card from './components/Card'
+
 import './App.css'
+import { ThemeProvider } from './context/theme'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [defaultTheme, setDefaultTheme] = useState('light')
+
+  const darkTheme = () => {
+    setDefaultTheme('dark')
+  }
+
+  const lightTheme = () => {
+    setDefaultTheme('light')
+  }
+  const toggleTheme = () => {
+    if (defaultTheme === 'light') {
+      darkTheme()
+    } else {
+      lightTheme()
+    }
+  }
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove('light', 'dark')
+    document.querySelector('html').classList.add(defaultTheme)
+  }, [defaultTheme])
 
   return (
+    <ThemeProvider value={{defaultTheme, darkTheme, lightTheme, toggleTheme}}>
     <>
-     
 <div className="flex flex-wrap min-h-screen items-center">
                 <div className="w-full">
                     <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
                         {/* Theme Toggle Button */}
+                        <ThemeButton />
                     </div>
 
                     <div className="w-full max-w-sm mx-auto">
                       {/* Card */}
-                       hello
+                        <Card />
+                       
                     </div>
                 </div>
             </div>
-
     </>
+    </ThemeProvider>
   )
 }
 
