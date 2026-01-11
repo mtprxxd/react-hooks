01320e2a78b50b288d39f7a71c9b5c01cce5d935
mtprxxd <109460576+mtprxxd@users.noreply.github.com>
2026-01-11 15:34:05 +0000

    new functionality added

--- Todo-Practice2/src/components/Form.jsx
+++ Todo-Practice2/src/components/Form.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {useTodo} from '../contexts/TodoContext'
-import {useState} from 'react'
+import {useState , useRef, useCallback} from 'react'
 
 function Form() {
     
@@ -14,21 +14,42 @@ function Form() {
         setTodo("")
     }
 
+    const textReference = useRef(null)
+
+const copyToClipboard = (e) => {
+  e.preventDefault()
+
+  if (!textReference.current) return
+
+  const value = textReference.current.value
+  if (!value) return
+
+  textReference.current.select()
+  navigator.clipboard.writeText(value)
+}
+
+
   
     
 
     return (
-        <form  onSubmit = {add} className="flex">
-            <input
-            value = {todo}
+        <form   className="flex">
+            <input 
+            
             onChange = {(e) => setTodo(e.target.value)}
                 type="text"
+                ref={textReference}
+                value={todo}
                 placeholder="Write Todo..."
                 className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
             />
-            <button  type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
+            <button onClick = {add} value = {todo}  type="click" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
                 Add
             </button>
+            <button  type="button" onClick = {copyToClipboard} className="bg-orange-500 text-white px-4 py-1 rounded-lg ml-2">
+                Copy
+            </button>
+            
         </form>
     );
 }
